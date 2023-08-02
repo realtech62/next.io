bb431a1377514886a915e648ae1721ecef305c31
realtech62 <user62@realtechltd.com>
2023-08-02 11:09:42 +0700

    Added links create form

---
+++ src/app/links/createForm.js
@@ -0,0 +1,34 @@
+'use client'
+import { useState } from "react"
+
+export default function LinksCreateForm() {
+	const [results, setResults] = useState(null)
+	const handleForm = async (event) => {
+		event.preventDefault()
+		const formData = new FormData(event.target)
+		const data = Object.fromEntries(formData)
+		const JSONData = JSON.stringify(data)
+		const endpoint = "/api/links"
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSONData
+		}
+		
+		const response = await fetch(endpoint, options)
+		const result = await response.json()
+		
+		setResults(result)
+	}
+
+	return <>
+		<form onSubmit={handleForm}>
+			<input type="text" name="url" placeholder="Your url to shorten" className="dark:bg-slate-800 dark:text-white px-2" defaultValue="https://github.com/realtech62/next.io"/>
+			<button type="submit">Shorten</button>
+		</form>
+		{results && <pre>{JSON.stringify(results, null, 4)}</pre>}
+	</>
+}
